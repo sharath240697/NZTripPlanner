3457b8f97206fdd9aceddc85bff71ce02b49ea26
chaithra123 <chaithrakharvi@gmail.com>
2020-04-10 16:14:20 +0530

    new Components Nav and search bar

---
+++ react-app/src/Components/NavBar/NavigationBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import styled from 'styled-components';
+const Styles = styled.div`
+  .navbar { background-color: #222; }
+  a, .navbar-nav, .navbar-light .nav-link {
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .form-center {
+    position: absolute !important;
+    left: 25%;
+    right: 25%;
+  }
+`;
+export const NavigationBar = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">NZ Trip Plan</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+       <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item><Nav.Link href="/">Plan Trip</Nav.Link></Nav.Item> 
+          <Nav.Item><Nav.Link href="/about">Saved Trip</Nav.Link></Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+)
+
+export default NavigationBar;
