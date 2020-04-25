2163bfe29e70f2593efcd9b576dfcbf5127d5594
sharath240697 <b.sharath240697@gmail.com>
2020-04-25 20:19:50 +1200

    feature upgradation express api & redux thunk

--- react-app/src/Actions/actions.js
+++ react-app/src/Actions/actions.js
@@ -1,5 +1,7 @@
 export const CHANGE_FROM_PLACE = 'CHANGE_FROM_PLACE'
 export const CHANGE_TO_PLACE = 'CHANGE_TO_PLACE'
+export const LOADING = 'LOADING';
+export const TESTDATA = 'TESTDATA';
 
 export const updateFromPlace = from => ({
     type: CHANGE_FROM_PLACE,
@@ -10,3 +12,17 @@ export const updateFromPlace = from => ({
     type: CHANGE_TO_PLACE,
     payload: to,
   })
+
+  export const loading = () => {
+    console.log('in actions.js ==>  loading method')
+    return {
+    type: LOADING,
+  }}
+
+  export const gettestdata = (data) => {
+    console.log('in actions.js ==>  gettestdata method')
+    return {
+    type: TESTDATA,
+    payload: data
+  }
+} 
