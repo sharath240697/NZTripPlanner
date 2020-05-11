7a17460ecc15e76902622dac890c53063fc649b4
sharath240697 <b.sharath240697@gmail.com>
2020-05-11 16:17:58 +1200

    Backend is modified such that it includes image url in the nearbny places raw data

--- react-app/src/Components/NearbyPlaces/NearbyPlaces.js
+++ react-app/src/Components/NearbyPlaces/NearbyPlaces.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import "./NearbyPlaces.css";
 import { Card, CardMedia, CardContent, CardActions, Button } from "@material-ui/core";
 import { Add, Remove } from '@material-ui/icons';
-const mapStateToProps = (state) => ({
+const  mapStateToProps = (state) => ({
   attractions: state.places.nearby_tourist_attractions.attractions,
 });
 const mapDispatchToProps = {};
@@ -12,26 +12,27 @@ const mapDispatchToProps = {};
 const NearbyPlaces = (props) => {
   return (
     <div>
-      {props.attractions.map((attraction) => (
-        <Card className="PlaceCard">
-          <CardMedia className="Media" image="https://via.placeholder.com/150" />
-          <div className="Content">
-            <CardContent >
-              <h3>{attraction.name}</h3>
-
-              <p>Rating: {attraction.rating}/5 from {attraction.user_ratings_total} users.</p>
-            </CardContent>
-            <CardActions>
-              <Button size="small" color="primary">
-                <Add />
-              </Button>
-              <Button size="small" color="primary">
-                <Remove />
-              </Button>
-            </CardActions>
-          </div>
-        </Card>
-      ))}
+      {props.attractions.map( (attraction) => { 
+        return (
+          <Card className="PlaceCard">
+            <CardMedia className="Media" image= {attraction.img_url} />
+            <div className="Content">
+              <CardContent >
+                <h3>{attraction.name}</h3>
+                <p>Rating: {attraction.rating}/5 from {attraction.user_ratings_total} users.</p>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  <Add />
+                </Button>
+                <Button size="small" color="primary">
+                  <Remove />
+                </Button>
+              </CardActions>
+            </div>
+          </Card>
+        )
+      })}
     </div>
   );
 };
