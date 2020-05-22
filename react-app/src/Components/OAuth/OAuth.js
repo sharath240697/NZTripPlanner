45b8e65c085834248b4bb6ef6620a00abf6f9a44
jwar663 <32050015+jwar663@users.noreply.github.com>
2020-05-22 12:00:54 +1200

    Converted OAuth.js to be a functional component, rather than stateful

--- react-app/src/Components/OAuth/OAuth.js
+++ react-app/src/Components/OAuth/OAuth.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import GoogleLogin from 'react-google-login'
 import './OAuth.css';
 import { store } from '../../index'
@@ -13,60 +13,44 @@ const mapStateToProps = (state) => ({
     credentials: state.oauth.Credentials
 });
 
+export function OAuth (props) {
 
-export class OAuth extends Component {
+    const [name, setName] = useState(undefined);
+    const [accessToken, setAccessToken] = useState(undefined);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [LoginDetails, setLoginDetails] = useState(undefined);
+    
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: undefined,
-            loggedIn: false,
-            accessToken: undefined,
-            idToken: undefined,
-            googleId: undefined
-        };
-    }
-    responseGoogle = (response) => {
-        // console.log(response);
-        // console.log(response.profileObj);
+    const responseGoogle = (response) => {
         console.log("inside responseGoogle function ")
         if (response.profileObj === undefined) {
-            this.setState({ name: undefined })
-            this.setState({ accessToken: undefined })
-            this.setState({ idToken: undefined })
-            this.setState({ googleId: undefined })
-            this.setState({ loggedIn: false })
+            setName(undefined)
+            setAccessToken(undefined)
+            setLoggedIn(false )
         } else {
-            this.setState({ name: response.profileObj.name })
-            this.setState({ accessToken: response.tokenObj.access_token })
-            this.setState({ googleId: response.profileObj.googleId })
-            this.setState({ idToken: response.tokenObj.id_token })
-            this.setState({ loggedIn: true })
+            setName(response.profileObj.name)
+            setAccessToken(response.tokenObj.access_token)
+            setLoggedIn(true)
         }
-        console.log("accessToken: " + this.state.accessToken);
-        console.log("logged in: " + this.state.loggedIn);
-        console.log("name: " + this.state.name);
-        console.log("idToken: " + this.state.idToken);
-        console.log("googleId: " + this.state.googleId);
+        console.log("accessToken: " + accessToken);
+        console.log("logged in: " + loggedIn);
+        console.log("name: " + name);
         store.dispatch(saveOAuthData(response));
     }
-    render() {
-        if (this.state.name === undefined) {
+        if (name === undefined) {
             return (
                 <div>
                     <GoogleLogin
                         clientId="4273262105-e7eogru655unj4t3q20ii204dvk8u397.apps.googleusercontent.com"
-                        buttonText="Save trip"
-                        onSuccess={this.responseGoogle}
-                        onFailure={this.responseGoogle}
+                        buttonText="Sign In"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
                         cookiePolicy={'single_host_origin'}
                         scope="https://www.googleapis.com/auth/drive.file"
-                        name={this.state.name}
-                        loggedIn={this.state.loggedIn}
-                        accessToken={this.state.accessToken}
-                        idToken={this.state.idToken}
-                        googleId={this.state.googleId}
-                        onClick={this.LoginDetails}
+                        name={name}
+                        loggedIn={loggedIn}
+                        accessToken={accessToken}
+                        onClick={LoginDetails}
                     />
                 </div>
             )
@@ -75,23 +59,20 @@ export class OAuth extends Component {
                 <div>
                     <GoogleLogin
                         clientId="4273262105-e7eogru655unj4t3q20ii204dvk8u397.apps.googleusercontent.com"
-                        buttonText={"Signed In As " + this.state.name}
-                        onSuccess={this.responseGoogle}
-                        onFailure={this.responseGoogle}
+                        buttonText={"Signed In As " + name}
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
                         cookiePolicy={'single_host_origin'}
-                        name={this.state.name}
-                        loggedIn={this.state.loggedIn}
-                        accessToken={this.state.accessToken}
-                        idToken={this.state.idToken}
-                        googleId={this.state.googleId}
+                        name={name}
+                        loggedIn={loggedIn}
+                        accessToken={accessToken}
                         scope="https://www.googleapis.com/auth/drive.file"
-                        onClick={this.LoginDetails}
+                        onClick={LoginDetails}
                     />
                 </div>
             )
         }
 
     }
-}
 //export default OAuth
 export default connect(mapStateToProps, mapDispatchToProps)(OAuth);
