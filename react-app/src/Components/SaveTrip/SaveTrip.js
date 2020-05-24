9b2b071a3564ec3a4c6029a7e0fbcf72e1b17cde
jake-good <jgoo479@aucklanduni.ac.nz>
2020-05-24 12:26:35 +1200

    Connects save trip button to express action calling save

--- react-app/src/Components/SaveTrip/SaveTrip.js
+++ react-app/src/Components/SaveTrip/SaveTrip.js
@@ -3,6 +3,22 @@ import React, { Component } from 'react'
 import { store } from '../../index'
 import { connect } from "react-redux";
 import { Button, TextField, Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText } from "@material-ui/core"
+import { postsavetrip } from '../../Actions/expressActions';
+import { v4 as uuidv4 } from 'uuid';
+
+const mapStateToProps = (state) => ({
+    name: state.oauth.Credentials.name,
+    accessToken: state.oauth.Credentials.accessToken,
+    loggedIn: state.oauth.Credentials.loggedIn,
+    LoginDetails: state.oauth.Credentials.LoginDetails,
+    expiry: state.oauth.Credentials.expiry,
+    credentials: state.oauth.Credentials,
+    placesOnMap: state.places.placesOnMap
+});
+
+const mapDispathToProps = {
+    postsavetrip
+};
 
 class SaveTrip extends Component {
     constructor(props) {
@@ -21,8 +37,24 @@ class SaveTrip extends Component {
 
     handleSave() {
         // STUB call express endpoint to save the trip from here 
-
+        const trip = this.buildTrip();
+        console.log(trip);
+        this.props.postsavetrip({ credentials: this.props.credentials.response.wc, trip: trip })
         this.handleClose();
+    }
+
+    buildTrip() {
+        const id = uuidv4();
+        const trip = {
+            id: id,
+            trip: {
+                placesOnMap: this.props.placesOnMap,
+                to: "to",
+                from: "from",
+                name: "name"
+            }
+        }
+        return trip;
     }
 
     handleClose() {
@@ -65,12 +97,6 @@ class SaveTrip extends Component {
     }
 }
 
-const mapStateToProps = (state) => ({
-    name: state.oauth.Credentials.name,
-    accessToken: state.oauth.Credentials.accessToken,
-    loggedIn: state.oauth.Credentials.loggedIn,
-    LoginDetails: state.oauth.Credentials.LoginDetails,
-    expiry: state.oauth.Credentials.expiry
-});
 
-export default connect(mapStateToProps)(SaveTrip);
+
+export default connect(mapStateToProps, mapDispathToProps)(SaveTrip);
