4ac819bad9893a98a50033db024f670939ff43d5
jake-good <jgoo479@aucklanduni.ac.nz>
2020-05-24 19:48:43 +1200

    Adds  message if viewing saved trips not logged in.
    If the user creds not in store conditionally display message.

--- react-app/src/Components/SavedTrip/SavedTrip.js
+++ react-app/src/Components/SavedTrip/SavedTrip.js
@@ -72,11 +72,13 @@ class SavedTrip extends Component {
 
   render() {
     return (
-      <div className="SavedTrip">
-        <TripTable setSelected={this.setSelectedTrip} trips={this.props.savedTrips} selected={this.state.selected} className="TripTable" />
-        <MapComponentDefault origin={this.state.selected.trip.from} destination={this.state.selected.trip.to.id} browser={{ lat: this.props.browser_lat, lng: this.props.browser_lng }} waypoints={this.state.selected.trip.placesOnMap} />
-        <NearbyPlaces />
-      </div>
+      this.props.credentials.response ? (
+        <div className="SavedTrip">
+          <TripTable setSelected={this.setSelectedTrip} trips={this.props.savedTrips} selected={this.state.selected} className="TripTable" />
+          <MapComponentDefault origin={this.state.selected.trip.from} destination={this.state.selected.trip.to.id} browser={{ lat: this.props.browser_lat, lng: this.props.browser_lng }} waypoints={this.state.selected.trip.placesOnMap} />
+          <NearbyPlaces />
+        </div>) :
+        (<p>Whoops... You need to log in with google to view saved trips</p>)
     );
   }
 }
